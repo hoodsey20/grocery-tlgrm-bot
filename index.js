31d508b48f9e22424453bfbd7606aad92b4967eb
hoodsey20 <hoodsey20@gmail.com>
2019-04-12 17:02:28 +0700

    добавил stage и scene для покупки

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const Telegraf = require('telegraf')
 const HttpsProxyAgent = require('https-proxy-agent')
+const Composer = require('telegraf/composer')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Markup = require('telegraf/markup')
+const WizardScene = require('telegraf/scenes/wizard')
+const { enter, leave } = Stage;
 
 let bot;
 
@@ -16,24 +22,68 @@ if (process.env.NODE_ENV === 'development') {
 
 // console.log("TCL: bot", bot)
 
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+// bot.start((ctx) => ctx.reply('Welcome!'))
+// bot.help((ctx) => ctx.reply('Send me a sticker'))
+
 bot.on('sticker', (ctx) => {
-    // console.log('ctx.update.message', ctx.update.message);
-    // ctx.reply(`I've got your sticker!`);
     console.log('ctx.update.message.sticker.file_id', ctx.update.message.sticker.file_id);
     ctx.replyWithSticker('CAADAgADEgADuDxkCPvWERlPuuDMAg'); 
-
 })
-bot.hears('hi', (ctx) => {
-	console.log("TCL: ctx", ctx.update.message.from)
-    ctx.reply('Кряк')
+// bot.hears('hi', (ctx) => {
+// 	console.log("TCL: ctx", ctx.update.message.from)
+//     ctx.reply('Кряк')
+// })
+
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+    return ctx.wizard.steps[ctx.wizard.cursor+1](ctx)
+})
+stepHandler.action('abort', (ctx) => {
+    ctx.replyWithMarkdown('ну ты даёшь!...я ушёл, если нужен то пиши /kupil')
+    return ctx.scene.leave()
+})
+stepHandler.use((ctx) => {
+    ctx.replyWithMarkdown('Ну и шо ты мне пишешь? Тыкони на кнопку')
+    return ctx.wizard.next()
 })
 
-bot.launch();   
- 
+const buyWizard = new WizardScene('kupil-wizard',
+  (ctx) => {
+    ctx.reply('Ну и шо купил?')
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    const purchasedItems = ctx.update.message.text;
+    ctx.scene.session.basketContent = purchasedItems; 
+    ctx.reply(`...ага, значит ${purchasedItems} купил. ну и шо, почём?`)
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    const price = ctx.update.message.text;
+    if (isNaN(Number(price)) || Number(price) < 1) {
+        ctx.replyWithMarkdown('погоди, это невалидная сумма...я ушёл, если нужен то пиши /kupil')
+        return ctx.scene.leave()
+    } 
+    ctx.reply(`ага, ну короч: ${ctx.scene.session.basketContent} за ${price}?`, Markup.inlineKeyboard([
+      Markup.callbackButton('👍 Ага', 'next'),
+      Markup.callbackButton('👎 Нит', 'abort')
+    ]).extra())
+    return ctx.wizard.next()
+  },
+  stepHandler,
+  (ctx) => {
+    console.log('last last last')
+    ctx.replyWithSticker('CAADAgADDAADuDxkCAACqyG7tH6DAg'); 
+    return ctx.scene.leave()
+  }
+)
+const stage = new Stage([buyWizard], { default: 'super-wizard' })
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.command('kupil', enter('kupil-wizard'))
+
 bot.catch((err) => {
     console.log('Ooops', err)
 })
-
-console.log('ok'); 
+bot.launch();   
